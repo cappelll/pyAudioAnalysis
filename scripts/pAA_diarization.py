e2fb6d17df3e8e0bfad517c880deea497dd7dd40
Luca Cappelletta <luca.cappelletta@recordsure.com>
2016-11-08 18:11:33 +0000

    pAA_diarization.py can ingest list files

--- scripts/pAA_diarization.py
+++ scripts/pAA_diarization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import copy
 import os
 import argparse
 
@@ -42,61 +43,93 @@ def ST_parse_args(parser, fInRefName, fInHypName, fOutDERName):
     parser.set_defaults(collar=0.0)
     parser.set_defaults(der_json_output=fOutDERName)
     parser.set_defaults(hyp=fInHypName)
+    parser.set_defaults(ref=fInRefName)
 
 
     return parser.parse_args()
 
 
+class Diarization():
 
-def diarization(parser):
+    def __init__(self, pOut=None, mtStep=0.1, LDAdim=0, doPlot=False):
 
-    args = parser.parse_args()
+        self.pOut = pOut
+        self.mtStep = mtStep
+        self.LDAdim = LDAdim
+        self.doPlot = doPlot
+        self.numberOfSpeakers = {}
 
-    fInAudioName = args.audio
-    pOut = args.outdir
-    numberOfSpeakers = args.speaker_number
-    fInRefName = args.ref
-    LDAdim = args.lda
-    doPlot = args.plot
+        self.DERs = {}
+        self.CLRs = {}
+        self.DIAs = {}
+        self.pAAScores = {}
 
-    mtStep = 0.1
-
-    fName = lsdutils.getFileBasename(fInAudioName)
-
-    if pOut is None:
-        fOutDiarizationName = None
-        fOutDERName = None
-
-    else:
-        fOutDiarizationName = os.path.join(pOut, fName + ".diarization.json")
-        fOutDERName = os.path.join(pOut, fName + ".DER.json")
+#        self._defaultsDERParser()
 
 
-    cls = aS.speakerDiarization(fInAudioName, numberOfSpeakers, mtStep=mtStep, LDAdim=LDAdim, PLOT=doPlot)
-    _times, _speakers = aS.flags2segs(cls, mtStep)
-    slices = [ slicelib.Slice(start=_s, end=_e, label="S{}".format(int(_sp))) for (_s, _e), _sp in zip(_times, _speakers)]
-    slicelib.writeJson(slices, fOutDiarizationName)
+    def _defaultsDERParser(self, collar=0.0):
 
-    if fInRefName:
+        self.DERParser = argparse.ArgumentParser()
+        self.DERParser.set_defaults(hyptype='diarization.json')
+        self.DERParser.set_defaults(reftype='SERP.tsv')
+        self.DERParser.set_defaults(mode='DER')
+        self.DERParser.set_defaults(nophrasemod=False)
+        self.DERParser.set_defaults(collar=collar)
 
-        scores = pAA_scores(fInRefName, cls, mtStep)
-        print "Cluster purity: {0:.1f}% - Speaker purity: {1:.1f}%".format(100*scores['purityClusterMean'], 100*scores['puritySpeakerMean'])
+#        _a  = self.DERParser.parse_args()
 
-        args_ST = ST_parse_args(parser, fInRefName, fOutDiarizationName, fOutDERName)
-        scoreTopics.main(args_ST)
+    def updateDERParser(self, fInRefName, fName):
 
+        parser = copy.copy(self.DERParser)
+        parser.set_defaults(ref=fInRefName)
+        parser.set_defaults(der_json_output=self.DERs[fName])
+        parser.set_defaults(hyp=self.DIAs[fName])
+
+        return parser
+
+    def diarization(self, fInAudioName, numberOfSpeakers=0):
+
+        fName = lsdutils.getFileBasename(fInAudioName)
+        self.numberOfSpeakers[fName] = numberOfSpeakers
+
+        if self.pOut is None:
+            self.DIAs[fName] = None
+            self.DERs[fName] = None
+
+        else:
+            self.DIAs[fName] = os.path.join(self.pOut, fName + ".diarization.json")
+            self.DERs[fName] = os.path.join(self.pOut, fName + ".DER.json")
+
+
+        self.CLRs[fName] = aS.speakerDiarization(fInAudioName, self.numberOfSpeakers[fName], mtStep=self.mtStep, LDAdim=self.LDAdim, PLOT=self.doPlot)
+        _times, _speakers = aS.flags2segs(self.CLRs[fName], self.mtStep)
+        slices = [ slicelib.Slice(start=_s, end=_e, label="S{}".format(int(_sp))) for (_s, _e), _sp in zip(_times, _speakers)]
+        slicelib.writeJson(slices, self.DIAs[fName])
+
+        return fName
+
+
+    def scoring(self, fInRefName, fName, parser):
+
+        self.pAAScores[fName] = pAA_scores(fInRefName, self.CLRs[fName], self.mtStep)
+        print "Cluster purity: {0:.1f}% - Speaker purity: {1:.1f}%".format(100*self.pAAScores[fName]['purityClusterMean'], 100*self.pAAScores[fName]['puritySpeakerMean'])
+
+        _p = ST_parse_args(parser, fInRefName, self.DIAs[fName], self.DERs[fName])
+#        parser = self.updateDERParser(fInRefName, fName)
+        scoreTopics.main(_p)
 
 
 def main():
 
-    global parser
-    parser = argparse.ArgumentParser(description='pyAudioAnalysis Diarization.')
 
-    parser.add_argument('-a','--audio', help='Input audio file', required=True)
+    parser = argparse.ArgumentParser(description='pyAudioAnalysis Diarization.')
+    parser.add_argument('-a','--audio', help='Input audio file', default=None)
+    parser.add_argument('--audio-list', help='Input audio file list', default=None)
     parser.add_argument('-o','--outdir', help='Output directory', required=True)
-    parser.add_argument('--ref', help='Reference file', default=None)
     parser.add_argument('-n','--speaker-number', help='Output directory', type=int, default=0)
     parser.add_argument("--lda", help="FLsD value", type=int, default=0)
+    parser.add_argument('--ref', help='Reference file', default=None)
+    parser.add_argument('--ref-list', help='Reference file', default=None)
     parser.add_argument("--plot", help="Plot", action="store_true", default=False)
 
 
@@ -104,10 +137,62 @@ def main():
         parser.print_help()
         sys.exit(0)
 
-    diarization(parser)
+    args = parser.parse_args()
+
+    fInAudioName = args.audio
+
+    fInAudioLstName = args.audio_list
+    pOut = args.outdir
+    numberOfSpeakers = args.speaker_number
+    fInRefName = args.ref
+    fInRefLstName = args.ref_list
+    LDAdim = args.lda
+    doPlot = args.plot
+
+    mtStep = 0.1
+
+
+    dia = Diarization(pOut=pOut, mtStep=mtStep, LDAdim=LDAdim, doPlot=doPlot)
+
+    if fInAudioName is not None:
+        fName = dia.diarization(fInAudioName)
+        if fInRefName is not None:
+            dia.scoring(fInRefName, fName)
+
+
+    if fInAudioLstName is not None:
+        audios = lsdutils.getFIlesFromList(fInAudioLstName)
+
+        if fInRefLstName is not None:
+            refs = lsdutils.getFIlesFromList(fInRefLstName)
+
+        for k in audios:
+
+            dia.diarization(audios[k])
+
+            if refs and k in refs:
+                dia.scoring(refs[k], k, parser)
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
