f49ac74cbbbfff1b3b3f38291a6f811317f88dd3
Luca Cappelletta <luca.cappelletta@recordsure.com>
2016-11-10 13:50:59 +0000

    pAA_diarization_wrapper.sh added

---
+++ scripts/pAA_diarization_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pAA_scrip_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+ACTIVATE="${pAA_scrip_DIR}/../venv/bin/activate"
+
+echo $ACTIVATE
+
+if [ -f $ACTIVATE ]; then
+    echo activate venv
+    source $ACTIVATE
+else
+    echo venv does NOT exist
+fi
+
+echo ${pAA_scrip_DIR}/pAA_diarization.py $@
+
+
